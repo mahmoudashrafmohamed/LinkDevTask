a5b7455dc36027f774bac7ab55d8434062e6be85
mahmoud_ashraf <mahmoud.ashraf@orcas.io>
2022-06-23 02:18:57 +0200

    add homeFragment UI

---
+++ app/src/main/java/com/mahmoudashraf/linkdevtask/home/fragment/HomeFragment.kt
@@ -0,0 +1,18 @@
+package com.mahmoudashraf.linkdevtask.home.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mahmoudashraf.linkdevtask.R
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+}
