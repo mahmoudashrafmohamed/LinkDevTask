a87ed1123d7f3a093c5c5432b67b12fa712389e7
mahmoud_ashraf <mahmoud.ashraf@orcas.io>
2022-06-23 02:57:46 +0200

    add core module and navigation component

--- app/src/main/java/com/mahmoudashraf/linkdevtask/MainActivity.kt
+++ app/src/main/java/com/mahmoudashraf/linkdevtask/MainActivity.kt
@@ -2,8 +2,13 @@ package com.mahmoudashraf.linkdevtask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mahmoudashraf.core.views.viewBinding
+import com.mahmoudashraf.linkdevtask.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by viewBinding(ActivityMainBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
